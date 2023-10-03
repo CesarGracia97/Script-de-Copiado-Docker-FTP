b3fec9d9dcca81ab42fa77a5f116a38bdd498c16
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2023-10-03 11:40:39 -0500

    Cambios: Primero Obtener ID contenedor

--- DockerContent_Copy.sh
+++ DockerContent_Copy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
-CONTAINER_DIR="/usr/src/app/entity/bankdebits"
-HOST_DIR="/home/root_wso/RespaldoEnviados"
-
-# Obtener el ID del contenedor que deseas utilizando el comando docker ps y grep
 CONTAINER_NAME_OR_ID=$(sudo docker ps | grep build | awk '{print $1}')
 
 if [ -z "$CONTAINER_NAME_OR_ID" ]; then
     echo "No se encontró ningún contenedor con 'build' en su nombre o ID."
     exit 1
 fi
+
+CONTAINER_DIR="/usr/src/app/entity/bankdebits"
+HOST_DIR="/home/root_wso/RespaldoEnviados"
+
+# Obtener el ID del contenedor que deseas utilizando el comando docker ps y grep
+
 
 sudo docker cp "${CONTAINER_NAME_OR_ID}:${CONTAINER_DIR}" "${HOST_DIR}"
 
@@ -23,4 +24,3 @@ echo "Archivos copiados desde el contenedor a ${HOST_DIR}"
 # 0 */12 * * * /ruta/al/script/copiar_archivos.sh                     (si es 12h)
 # 0 5 * * * TZ='America/Guayaquil' /ruta/al/script.sh
 # 25 16 * * * TZ='America/Guayaquil' /ruta/al/script.sh
-
